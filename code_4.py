cfb03d0fe4936d0eb314a4a9a1b62102e60470e8
Maine <157335221+jaterra@users.noreply.github.com>
2024-02-23 20:41:30 -0700

    Update

--- code_4.py
+++ code_4.py
@@ -5,50 +5,64 @@ big_mac_file = './big-mac-full-index.csv'
 df = pd.read_csv(big_mac_file)
 
 def get_big_mac_price_by_year(year, country_code):
-    filtered_data = df[(df['date'].str.startswith(f"{year}-")) & (df['iso_a3'] == country_code)]['dollar_price']
-    mean_price = filtered_data.mean() if not filtered_data.empty else 0
-    return round(mean_price, 2)
+   
+    query = f"(iso_a3 == '{country_code.upper()}')"
+    country_df = df.query(query)
+    country_df = country_df[country_df['date'].apply(lambda row: row[:4] == str(year))]
+
+    average_price = round(country_df['dollar_price'].mean(), 2)
+
+    return average_price
 
 def get_big_mac_price_by_country(country_code):
-    filtered_data = df[df['iso_a3'] == country_code]['dollar_price']
-    mean_price = filtered_data.mean() if not filtered_data.empty else 0
-    return round(mean_price, 2)
+
+    query = f"(iso_a3 == '{country_code.upper()}')"
+    country_df = df.query(query)
+
+    average_price = round(country_df['dollar_price'].mean(), 2)
+
+    return average_price
 
 def get_the_cheapest_big_mac_price_by_year(year):
-    filtered_data = df[df['date'].str.startswith(f"{year}-")].nsmallest(1, 'dollar_price')
     
-    if filtered_data.empty:
-        return f"No data available for {year}"
-    
-    country_name = filtered_data['name'].iloc[0]
-    country_code = filtered_data['iso_a3'].iloc[0]
-    price = round(filtered_data['dollar_price'].iloc[0], 2)
 
-    return f"In {year}, the cheapest Big Mac price is {country_name}({country_code.upper()}): ${price}"
+    query = df['date'].apply(lambda row: row[:4] == str(year))
+    year_df = df[query]
+
+    min_idx = year_df['dollar_price'].idxmin()
+    country_name = year_df.loc[min_idx]['name']
+    country_code = year_df.loc[min_idx]['iso_a3']
+    dollar_price = round(year_df.loc[min_idx]['dollar_price'], 2)
+
+    output = f"{country_name} ({country_code}): ${dollar_price}"
+
+    return output
 
 def get_the_most_expensive_big_mac_price_by_year(year):
-    filtered_data = df[df['date'].str.startswith(f"{year}-")].nlargest(1, 'dollar_price')
     
-    if filtered_data.empty:
-        return f"No data available for {year}"
-    
-    country_name = filtered_data['name'].iloc[0]
-    country_code = filtered_data['iso_a3'].iloc[0]
-    price = round(filtered_data['dollar_price'].iloc[0], 2)
+    query = df['date'].apply(lambda row: row[:4] == str(year))
+    year_df = df[query]
 
-    return f"In {year}, the most expensive Big Mac price is {country_name}({country_code.upper()}): ${price}"
+    max_idx = year_df['dollar_price'].idxmax()
+    country_name = year_df.loc[max_idx]['name']
+    country_code = year_df.loc[max_idx]['iso_a3']
+    dollar_price = round(year_df.loc[max_idx]['dollar_price'],2)
+
+    output = f"{country_name}({country_code}): ${dollar_price}"
+    
+    return output
 
 if __name__ == "__main__":
-    year = 2014
-    country_code = 'mex'
+   
 
-    print(f"Mean Big Mac price in {year} in {country_code.upper()} is: ${get_big_mac_price_by_year(year, country_code)}")
-    print(f"Mean Big Mac price in {country_code.upper()} is: ${get_big_mac_price_by_country(country_code)}")
-    print(get_the_cheapest_big_mac_price_by_year(year))
-    print(get_the_most_expensive_big_mac_price_by_year(year))
+    result_a = get_big_mac_price_by_year(2010, "arg")
+    print(result_a)
+    result_b = get_big_mac_price_by_country("mex")
+    print(result_b)
+    result_c = get_the_cheapest_big_mac_price_by_year(2008)
+    print(result_c)
+    result_d = get_the_most_expensive_big_mac_price_by_year(2014)
+    print(result_d)
 
 
 
-
-
-    
--- pandas_sandbox.py
+++ pandas_sandbox.py
@@ -26,19 +26,173 @@
 # query_text = f"iso_a == @country_code"
 # query = f"iso_a3 == 'USA'"
 
+# import pandas as pd
+
+# df = pd.read_csv('big-mac-full-index.csv')
+# print(len(df))
+# print(df.index)
+# print(len(df.index))
+# print (type(df.index))
+
+# numbers = range(1631)
+# print(numbers)
+
+# for i in df.index:
+#     print(i)
+#     print(df['dollar_price'][i])
+#     print(df.loc[i]['name'])
+#     print(type(df.loc[i]))
+
+
+
+# import pandas as pd
+# fruits = ['apple','melon','grapes','orange','kiwi']
+# fruit_series = pd.Series(fruits)
+
+# print('Fruits')
+# print(fruits)
+# print('Fruits series')
+# print(fruit_series)
+
+
+# import pandas as pd
+
+# students = {"Texas":"Jazmin","Alabama":"Roberto","Virginia":"David","California":"Alex"}
+# student_series = pd.Series(data=students.values(), index=students.keys())
+
+# print("Students")
+# print(students)
+# print("Student Series")
+# print(student_series)
+
+
+# print(fruit_series.index)
+# print(student_series.index)
+
+# print(fruit_series.index.values)
+
+
+
+# import pandas as pd
+
+# filename = "./big-mac-full-index.csv"
+# df = pd.read_csv(filename)
+
+# print(df.columns)
+# print(df)
+# print(df['dollar_price'])
+
+
+
+# Pandas queries
+# Prints all of MEX rows in file
+# import pandas as pd
+
+# filename = "./big-mac-full-index.csv"
+# df = pd.read_csv(filename)
+
+# query = f"(iso_a3 == 'MEX')"
+# mxn_df = df.query(query)
+
+# print(mxn_df)
+
+
+
+# Pandas operations
+
+# import pandas as pd
+
+# filename = "./big-mac-full-index.csv"
+# df = pd.read_csv(filename)
+
+# query = f"(iso_a3 == 'MEX')"
+# mxn_df = df.query(query)
+
+# print(mxn_df.median())
+
+
+
+# import pandas as pd
+
+# filename = "./big-mac-full-index.csv"
+# df = pd.read_csv(filename)
+
+# query = f"(iso_a3 == 'MEX')"
+# mxn_df = df.query(query)
+
+# print(type(mxn_df['dollar_price'].median()))
+
+
+
+# MEAN
 import pandas as pd
 
-df = pd.read_csv('big-mac-full-index.csv')
-print(len(df))
-print(df.index)
-print(len(df.index))
-print (type(df.index))
+filename = "./big-mac-full-index.csv"
+df = pd.read_csv(filename)
 
-numbers = range(1631)
-print(numbers)
+print(df['dollar_price'])
 
-for i in df.index:
-    print(i)
-    print(df['dollar_price'][i])
-    print(df.loc[i]['name'])
-    print(type(df.loc[i]))
+query = f"(iso_a3 == 'MEX')"
+mxn_df = df.query(query)
+
+print(mxn_df['dollar_price'].min())
+print(mxn_df['dollar_price'].max())
+print(round(mxn_df['dollar_price'].mean(), 2))
+
+
+
+# prints min dollar price in japan
+# import pandas as pd
+
+# filename = "./big-mac-full-index.csv"
+# df = pd.read_csv(filename)
+
+# query = f"(iso_a3 == 'JPN')"
+# jpn_df = df.query(query)
+# min_idx = jpn_df['dollar_price'].idxmin()
+
+# print(min_idx)
+# print(jpn_df.loc[min_idx])
+# print(jpn_df.loc[min_idx]['dollar_price'])
+
+
+
+# prints max dollar price in argentina
+# import pandas as pd
+
+# filename = "./big-mac-full-index.csv"
+# df = pd.read_csv(filename)
+
+# query = f"(iso_a3 == 'ARG')"
+# arg_df = df.query(query)
+# max_idx = arg_df['dollar_price'].idxmax()
+
+# print(max_idx)
+# print(arg_df.loc[max_idx])
+# print(arg_df.loc[max_idx]['dollar_price'])
+
+
+
+# prints more vertically
+# import pandas as pd
+
+# filename = "./big-mac-full-index.csv"
+# df = pd.read_csv(filename)
+
+# query = f"(iso_a3 == 'ARG')"
+# arg_df = df.query(query)
+
+# for index, row in arg_df.iterrows():
+#     print(index)
+#     print(row)
+
+
+# import pandas as pd
+
+# filename = "./big-mac-full-index.csv"
+# df = pd.read_csv(filename)
+
+# query = f"(iso_a3 == 'ARG')"
+# arg_df = df.query(query)
+
+# print(arg_df)
